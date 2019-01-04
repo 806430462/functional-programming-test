1fb0a56748016033ca551af70b7abd3199734cb6
wangpengz <wangpengz@yonyou.com>
2019-01-04 14:08:55 +0800

    init

---
+++ src/lambda/SimpleExample.java
@@ -0,0 +1,34 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+/* 常见的5种lambda
+ * // 1. 不需要参数,返回值为 5  
+ * () -> 5  
+ * // 2. 接收一个参数(数字类型),返回其2倍的值  
+ * x -> 2 * x  
+ * // 3. 接受2个参数(数字),并返回他们的差值  
+ * (x, y) -> x – y  
+ * // 4. 接收2个int型整数,返回他们的和  
+ * (int x, int y) -> x + y  
+ * // 5. 接受一个 string 对象,并在控制台打印,不返回任何值(看起来像是返回void)  
+ * (String s) -> System.out.print(s)  
+ */
+public class SimpleExample {
+	public static void main(String[] args) {
+		String[] atp = { "Rafael Nadal", "Novak Djokovic", "Stanislas Wawrinka", "David Ferrer", "Roger Federer",
+				"Andy Murray", "Tomas Berdych", "Juan Martin Del Potro" };
+		List<String> players = Arrays.asList(atp);
+
+		// 以前的循环方式
+		for (String player : players) {
+			System.out.print(player + "; ");
+		}
+
+		// 使用 lambda 表达式以及函数操作(functional operation)
+		players.forEach((player) -> System.out.print(player + "; "));
+
+		// 在 Java 8 中使用双冒号操作符(double colon operator)
+		players.forEach(System.out::println);
+	}
+}
